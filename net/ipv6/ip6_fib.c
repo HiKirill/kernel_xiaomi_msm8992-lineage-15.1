c225f0e134d0c1cb432a45bc507e46deb31709a8
Eric Dumazet <edumazet@google.com>
2017-09-08 15:48:47 -0700

    ipv6: fix typo in fib6_net_exit()
    commit 32a805baf0fb70b6dbedefcd7249ac7f580f9e3b upstream.
    
    IPv6 FIB should use FIB6_TABLE_HASHSZ, not FIB_TABLE_HASHSZ.
    
    Fixes: ba1cc08d9488 ("ipv6: fix memory leak with multiple tables during netns destruction")
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Willy Tarreau <w@1wt.eu>

--- net/ipv6/ip6_fib.c
+++ net/ipv6/ip6_fib.c
@@ -1750,10 +1750,14 @@ static void fib6_net_exit(struct net *net)
 	del_timer_sync(&net->ipv6.ip6_fib_timer);
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 	for (i = 0; i < FIB6_TABLE_HASHSZ; i++) {
 =======
 	for (i = 0; i < FIB_TABLE_HASHSZ; i++) {
 >>>>>>> 4e39d5e48bac... ipv6: fix memory leak with multiple tables during netns destruction
+=======
+	for (i = 0; i < FIB6_TABLE_HASHSZ; i++) {
+>>>>>>> 488ec7b227e4... ipv6: fix typo in fib6_net_exit()
 		struct hlist_head *head = &net->ipv6.fib_table_hash[i];
 		struct hlist_node *tmp;
 		struct fib6_table *tb;
